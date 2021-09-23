bcdc93827e373c8523f1305274f57fdd42de27ca
Neha Yadav <nehayadav903@gmail.com>
2021-09-22 22:19:01 -0400

    jQuery connect to html

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+  $('textarea').keyup(function() {
+    
+    let count = $(this).val().length;
+
+    if (count <= 140) {
+      $(this)
+        .closest(".new-tweet")
+        .find(".counter")
+        .removeClass("negative-count")
+        .text(140 - count);
+    } else {
+      $(this)
+        .closest(".new-tweet")
+        .find(".counter")
+        .addClass("negative-count")
+        .text(140 - count);
+    }
+  });
+
+  const buttonToTriggerScroll = '.scroll-to-top button';
+  //For toggle button to scroll upwards
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 20) {
+      $(buttonToTriggerScroll).fadeIn();
+    } else {
+      $(buttonToTriggerScroll).fadeOut();
+    }
+  });
+
+  $(buttonToTriggerScroll).click(function() {
+    $(window).scrollTop(0);
+    if ($('.new-tweet').is(':hidden')) {
+      $('.new-tweet').show();
+    }
+    $('textarea').focus();
+  });
+});
